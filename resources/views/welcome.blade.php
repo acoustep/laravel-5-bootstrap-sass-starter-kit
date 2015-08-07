0a386bbd6769ac9965e4be4dc974247efe74b135
Mitch Stanley <mitch@thestudio4.co.uk>
2015-08-07 09:48:27 +0100

    remove swipebox example

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,7 +6,4 @@
   <p>...</p>
   <p><a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a></p>
 </div>
-<a href="big/image.jpg" class="swipebox" title="My Caption">
-	<img src="small/image.jpg" alt="image">
-</a>
 @endsection
